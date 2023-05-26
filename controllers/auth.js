51b445dd0fe6ff68a368d609e91c481bf6ca5e5e
mushegharustamyan <mushegh.arustamyan@gmail.com>
2023-05-26 03:22:29 -0700

    upd

--- controllers/auth.js
+++ controllers/auth.js
@@ -38,8 +38,6 @@ exports.refresh = async (req , res) => {
 
 	const {id} = jwt.decode(token)
 
-	console.log(id)
-
 	User.findByPk(id, {include: Role})
 	.then((user) => sendResBody(res , 200 , user))
 	.catch(() => sendResStatus(res, 500))
